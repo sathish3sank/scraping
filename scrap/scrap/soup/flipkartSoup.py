7e6cb762dacf91afff622216fbaa4ba5be017e71
SathishP <sathishpgit@gmail.com>
2022-05-15 13:21:22 +0530

    Scraping initial commmit

--- scrap/scrap/soup/flipkartSoup.py
+++ scrap/scrap/soup/flipkartSoup.py
@@ -1,43 +1,76 @@
 from constant import flipkart, FlipkartLiterals, Scrap
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag, NavigableString
+from constant import get_url
 
+def getTextFromSoup(soup):
+    if(soup):
+        return soup.text
+    else:
+        return ''
 
-from scrap.scrap.utils.index import get_url
+def getImageSrcFromSoup(soup):
+    if(soup):
+        return str(soup["src"])
+    else:
+        return ''
+
+def getURLFromSoup(soup):
+    if(soup):
+        return "https://www.flipkart.com"+ str(soup["href"])
+    else:
+        return ''
+
+def validateSoup(soup):
+    if soup != None:
+        return True
+    else:
+        return False
 
 def getTitleWithDivAndClass(soup : BeautifulSoup) -> str:
-    title : str = ''
-    for titleClass in flipkart.get(FlipkartLiterals.TITLE):
-        title += soup.find('div', attrs= { 'class' : titleClass })
-        break
-    return title != '' if title else 'None'
+    title = ''
+    for titleClass in flipkart.get(FlipkartLiterals.TITLE.value):
+        souped = soup.find('div', attrs= { 'class' : titleClass })
+        if(validateSoup(souped)):
+            title = souped
+            break
+    return getTextFromSoup(title)
 
 def getImageWithDivAndClass(soup : BeautifulSoup) -> str:
-    image : str = ''
-    for imgClass in flipkart.get(FlipkartLiterals.IMAGE):
-        image += soup.find('div', attrs= { 'class' : imgClass })
-        break
-    return image != '' if image else 'None'
+    image = ''
+    for imgClass in flipkart.get(FlipkartLiterals.IMAGE.value):
+        souped = soup.find("img", attrs={"class": imgClass})
+        if(validateSoup(souped)):
+            image = souped
+            break
+    return getImageSrcFromSoup(image)
 
 def getPriceWithDivAndClass(soup : BeautifulSoup) -> str:
-    price : str = ''
-    for priceClass in flipkart.get(FlipkartLiterals.PRICE):
-        price += soup.find('div', attrs= { 'class' : priceClass })
-        break
-    return price != '' if price else 'None'
+    price = ''
+    for priceClass in flipkart.get(FlipkartLiterals.PRICE.value):
+        souped = soup.find('div', attrs= { 'class' : priceClass })
+        if(validateSoup(souped)):
+            price = souped
+            break
+    return getTextFromSoup(price)
 
 def getURLWithDivAndClass(soup : BeautifulSoup) -> str:
-    url : str = ''
-    for urlClass in flipkart.get(FlipkartLiterals.URL):
-        url += soup.find('div', attrs= { 'class' : urlClass })
-        break
-    return url != '' if url else 'None'
+    url = ''
+    for urlClass in flipkart.get(FlipkartLiterals.URL.value):
+        souped = soup.find('a', attrs= { 'class' : urlClass })
+        if(validateSoup(souped)):
+            url = souped
+            break
+    return getURLFromSoup(url)
 
 def getDescriptionWithDivAndClass(soup : BeautifulSoup) -> str:
-    desc : str = ''
-    for descClass in flipkart.get(FlipkartLiterals.DESCRIPTION):
-        desc += soup.find('div', attrs= { 'class' : descClass })
-        break
-    return desc != '' if desc else 'None'
+    desc = ''
+    for descClass in flipkart.get(FlipkartLiterals.DESCRIPTION.value):
+        souped = soup.find("ul", attrs={"class": descClass})
+        if(validateSoup(souped)):
+            desc = souped
+            break
+    return getTextFromSoup(desc)
+
 
 def makeScrap(soup : BeautifulSoup) -> Scrap:
     return {
@@ -48,11 +81,16 @@ def makeScrap(soup : BeautifulSoup) -> Scrap:
         'url' : getURLWithDivAndClass(soup)
     }
 
-def flipkartScraping(url : str, soup : BeautifulSoup) -> Scrap:
+def flipkartScraping(url : str):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 11.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0',
         'Accept-Language': 'en-US, en;q=0.5'
     }
+    data = []
     res = get_url(url, headers)
     soup = BeautifulSoup(res.text, 'html.parser')
-    return makeScrap(soup)
+    for page in flipkart.get(FlipkartLiterals.PAGE.value):
+        for s in soup.find_all('div', attrs = { 'class' : page}):
+            data += [makeScrap(s)]
+    
+    return data
